498a097e067c8f0e84f5a4596eb003cca73e5fff
chidraeve <chidraeve@clubitsolutions.de>
2023-06-07 14:20:09 +0200

    imported solutions code from end of section. Gonna skip code-along for this chapter and going to view the differences so far

--- wp-content/themes/udemy-course-theme/index.php
+++ wp-content/themes/udemy-course-theme/index.php
@@ -1,23 +1,14 @@
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset=<?php bloginfo('charset');?>>
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
-	<style>
-		:lang(ar) {
-			direction: rtl;
-		}
-	</style>
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <?php wp_head(); ?>
 </head>
 <body <?php body_class('example'); ?>>
-	<?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 
-	<p>Hello World!</p>
+  <p>Hello world!</p>
 
-	<?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 </body>
 </html>
