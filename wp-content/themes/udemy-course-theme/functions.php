66abd013cc9f1f1c41e7d39f0e687bae0beb040d
chidraeve <chidraeve@clubitsolutions.de>
2023-06-07 10:45:11 +0200

    moved static folder into theme and renamed it to assets, added to sql files to repo, a test.php to test php code and modified .gitignore

---
+++ wp-content/themes/udemy-course-theme/functions.php
@@ -0,0 +1,9 @@
+<?php
+// Variables
+
+// Includes
+include_once(get_theme_file_path('/includes/front/enqueue.php'));
+include_once(get_theme_file_path('/includes/front/head.php'));
+// Hooks
+add_action('wp_enqueue_scripts', 'u_enqueue');
+add_action('wp_head', 'u_head', 5);
