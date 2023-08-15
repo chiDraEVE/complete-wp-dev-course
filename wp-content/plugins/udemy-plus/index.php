d134e49d23cadfc9d9d9f9101a1a3c5f551fc8e2
chidraeve <chidraeve@clubitsolutions.de>
2023-08-15 14:18:55 +0200

    forgot to add index.php in the last commit

---
+++ wp-content/plugins/udemy-plus/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name:       Udemy Plus
+ * Plugin URI:        https://fullstackdevelopment.de
+ * Description:       A plugin for adding blocks to a theme
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      7.2
+ * Author:            chiDraEVE
+ * Author URI:        https://clubitsolutions.de/
+ * Text Domain:       udemy-plus
+ * Domain Path:       /languages
+ */
