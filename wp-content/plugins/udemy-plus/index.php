09704e2352f499244f256586bd133c6a1e1d33de
chidraeve <chidraeve@clubitsolutions.de>
2023-08-15 14:51:29 +0200

    122. securing the main plugin file

--- wp-content/plugins/udemy-plus/index.php
+++ wp-content/plugins/udemy-plus/index.php
@@ -11,3 +11,8 @@
  * Text Domain:       udemy-plus
  * Domain Path:       /languages
  */
+
+if (!function_exists("add_action")) {
+    echo 'Seems like you stumbled here by accident. 😜';
+    exit;
+}
