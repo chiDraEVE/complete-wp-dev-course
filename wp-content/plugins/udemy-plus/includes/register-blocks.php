ee2a10e55bb37eb024accc0615a4659005f86ec1
chidraeve <chidraeve@clubitsolutions.de>
2023-08-16 14:07:12 +0200

    Hab mir den Rest des Kapitals aus den Quellen kopiert, dass ich Zigaretten stopfen kann während ich mir den Rest das Kapitels ansehe

--- wp-content/plugins/udemy-plus/includes/register-blocks.php
+++ wp-content/plugins/udemy-plus/includes/register-blocks.php
@@ -1,7 +1,13 @@
 <?php
 
 function up_register_blocks() {
+  $blocks = [
+    [ 'name' => 'fancy-header' ]
+  ];
+
+  foreach($blocks as $block) {
     register_block_type(
-        UP_PLUGIN_DIR . 'build/block.json'
+      UP_PLUGIN_DIR . 'build/blocks/' . $block['name']
     );
+  }
 }
--- wp-content/plugins/udemy-plus/index.php
+++ wp-content/plugins/udemy-plus/index.php
@@ -1,20 +1,20 @@
 <?php
-/*
+/**
  * Plugin Name:       Udemy Plus
- * Plugin URI:        https://fullstackdevelopment.de
- * Description:       A plugin for adding blocks to a theme
+ * Plugin URI:        https://udemy.com
+ * Description:       A plugin for adding blocks to a theme.
  * Version:           1.0.0
  * Requires at least: 5.9
  * Requires PHP:      7.2
- * Author:            chiDraEVE
- * Author URI:        https://clubitsolutions.de/
+ * Author:            Udemy
+ * Author URI:        https://udemy.com
  * Text Domain:       udemy-plus
  * Domain Path:       /languages
  */
 
-if (!function_exists("add_action")) {
-    echo 'Seems like you stumbled here by accident. 😜';
-    exit;
+if(!function_exists('add_action')) {
+  echo 'Seems like you stumbled here by accident. 😛';
+  exit;
 }
 
 // Setup
