f5096f6d37747f7c8a8eb22dbd3a60d4cfad3928
chidraeve <chidraeve@clubitsolutions.de>
2023-08-19 17:49:19 +0200

    144.Creating a Search Form Block

--- wp-content/plugins/udemy-plus/build/blocks/fancy-header/index.asset.php
+++ wp-content/plugins/udemy-plus/build/blocks/fancy-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '31c0e09ee2b37107226d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0a6499040ba4de5be33b');
