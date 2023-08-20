2be802132796daa9cb7f6edfc04dd6dc2ff68f91
chidraeve <chidraeve@clubitsolutions.de>
2023-08-20 16:54:49 +0200

    150. Rendering a Block with Output Buffers

--- wp-content/plugins/udemy-plus/includes/search-form.php
+++ wp-content/plugins/udemy-plus/includes/search-form.php
@@ -1,5 +1,22 @@
 <?php
 
 function up_search_form_render_cb() {
-    return 'Hello!';
+    echo 'hey';
+    ob_start();
+    ?>
+        <h1>Search: Your search term here</h1>
+        <form>
+            <input type="text" placeholder="Search" />
+            <div className="btn-wrapper">
+                <button type="submit" style={{
+                'background-color': bgColor,
+                color: textColor
+                }}>Search</button>
+            </div>
+        </form>
+<?php
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
 }
