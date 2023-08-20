72befd920417c91530bcd33fac42f464f236bafe
chidraeve <chidraeve@clubitsolutions.de>
2023-08-20 16:28:33 +0200

    148. Excercise: Applying Colors

--- wp-content/plugins/udemy-plus/build/blocks/search-form/index.asset.php
+++ wp-content/plugins/udemy-plus/build/blocks/search-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f273cde4ea95943f035a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '03efa87f28360ad121d9');
