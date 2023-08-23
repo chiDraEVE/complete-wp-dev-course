2e969a8eded9b0c68e61c2e4a19a3feb277a58c7
chidraeve <chidraeve@clubitsolutions.de>
2023-08-23 18:20:10 +0200

    156. The Toggle Control Component

--- wp-content/plugins/udemy-plus/src/blocks/page-header/index.js
+++ wp-content/plugins/udemy-plus/src/blocks/page-header/index.js
@@ -1,19 +1,30 @@
 import { registerBlockType } from '@wordpress/blocks';
 import {
-    useBlockProps, RichText
+    useBlockProps, RichText, InspectorControls
 } from '@wordpress/block-editor';
+
 import { __ } from '@wordpress/i18n'
 import icons from '../../icons.js'
 import './main.css'
+import {PanelBody, ToggleControl} from "@wordpress/components";
 
 registerBlockType('udemy-plus/page-header', {
     icon: icons.primary,
     edit({attributes, setAttributes}) {
+        const { content, showCategory } = attributes
         const blockProps = useBlockProps()
-        const { content } = attributes
 
         return (
             <>
+                <InspectorControls>
+                    <PanelBody title={__('General', 'udemy-plus')}>
+                        <ToggleControl
+                            label={__('Show Category', 'udemy-plus')}
+                            checked={showCategory}
+                            onChange={showCategory => setAttributes({ showCategory })}
+                        />
+                    </PanelBody>
+                </InspectorControls>
                 <div { ...blockProps }>
                     <div class="inner-page-header">
                         <RichText
