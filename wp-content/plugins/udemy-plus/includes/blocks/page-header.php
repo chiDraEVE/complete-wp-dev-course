86cc861c551db7159d96ee375ece36a9a0a3c762
chidraeve <chidraeve@clubitsolutions.de>
2023-08-24 14:19:09 +0200

    158. Rendering the Category

--- wp-content/plugins/udemy-plus/includes/blocks/page-header.php
+++ wp-content/plugins/udemy-plus/includes/blocks/page-header.php
@@ -1,6 +1,23 @@
 <?php
 
-function up_page_header_render_cb($atts) {
-    $output = esc_attr($atts['content']);
+function up_page_header_render_cb($atts): string {
+    $heading = esc_html($atts['content']);
+
+    if($atts['showCategory']) {
+        $heading = get_the_archive_title();
+    }
+
+    ob_start();
+    ?>
+    <div class="wp-block-udemy-plus-page-header">
+        <div class="inner-page-header">
+            <h1><?php echo $heading; ?></h1>
+        </div>
+    </div>
+    <?php
+
+    $output = ob_get_contents();
+    ob_end_clean();
+
     return $output;
 }
