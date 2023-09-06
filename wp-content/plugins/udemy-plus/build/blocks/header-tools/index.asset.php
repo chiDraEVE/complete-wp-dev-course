c9303da77f16f85b08e7e6ac495bc4268d712d8e
chidraeve <chidraeve@clubitsolutions.de>
2023-09-06 15:20:52 +0200

    166. Exercise: Toggling the Links

--- wp-content/plugins/udemy-plus/build/blocks/header-tools/index.asset.php
+++ wp-content/plugins/udemy-plus/build/blocks/header-tools/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6f6cf7580c23db503202');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b3e564d429c33704f9c5');
